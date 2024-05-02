0f8415c1f283214f314cccee3a887f94d531da2a
Will Nahley <nahley.will@gmail.com>
2024-05-02 00:36:56 -0500

    Refactored and outputted predictions for T = 300

--- hw4/logdouble.py
+++ hw4/logdouble.py
@@ -18,6 +18,10 @@ class logdouble:
         prod = logdouble(self.log_val + other.log_val, 1)
         return prod
     
+    def __truediv__(self, other):
+        div = logdouble(self.log_val - other.log_val, 1)
+        return div
+    
     def __add__(self, other):
         sum = 0.0
         if other.log_val <= self.log_val:
@@ -27,4 +31,12 @@ class logdouble:
         
         sum = logdouble(sum, True)
         return sum
+    
+    def __str__(self):
+        return str(self.log_val)
+    
+    def o(self):
+        return self.original_val
+    def e(self):
+        return self.log_val
     
